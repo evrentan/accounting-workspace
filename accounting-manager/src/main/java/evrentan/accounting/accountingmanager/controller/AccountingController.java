94ba44a6d4df4cf4259866764745fedaec7aadf8
Evren Tan <evrentan@jotform.com>
2023-07-03 16:48:26 +0300

    [Doc]: Add Missing Javadoc
    Add Missing Javadoc

--- accounting-manager/src/main/java/evrentan/accounting/accountingmanager/controller/AccountingController.java
+++ accounting-manager/src/main/java/evrentan/accounting/accountingmanager/controller/AccountingController.java
@@ -29,6 +29,16 @@ public class AccountingController {
         this.accountingService = accountingService;
     }
 
+    /**
+     * REST end-point in order to create an accounting item.
+     * Details related to API specs can be found in the API Documentation which can be reached as described in README file.
+     *
+     * @param createAccountingItemRequest object that is going to be created. Please, see the {@link CreateAccountingItemRequest} class for details.
+     * @return CreateAccountingItemResponse Object within ResponseEntity. Please, see the {@link CreateAccountingItemResponse} & {@link ResponseEntity} classes for details.
+     *
+     * @author <a href="https://github.com/evrentan">Evren Tan</a>
+     * @since 1.0.0
+     */
     @PostMapping
     @Operation(summary = "Create Accounting Item")
     @ApiResponses(value = {
@@ -42,6 +52,15 @@ public class AccountingController {
         return ResponseEntity.created(URI.create(createAccountingItemResponse.getId().toString())).body(createAccountingItemResponse);
     }
 
+    /**
+     * REST end-point in order to get all accounting items.
+     * Details related to API specs can be found in the API Documentation which can be reached as described in README file.
+     *
+     * @return GetAccountingListResponse Object within ResponseEntity. Please, see the {@link GetAccountingListResponse} & {@link ResponseEntity} classes for details.
+     *
+     * @author <a href="https://github.com/evrentan">Evren Tan</a>
+     * @since 1.0.0
+     */
     @GetMapping
     @Operation(summary = "Get All Accounting Items")
     @ApiResponses(value = {
--- accounting-manager/src/main/java/evrentan/accounting/accountingmanager/repository/AccountingRepository.java
+++ accounting-manager/src/main/java/evrentan/accounting/accountingmanager/repository/AccountingRepository.java
@@ -15,5 +15,16 @@ import java.util.UUID;
  */
 @Repository
 public interface AccountingRepository extends JpaRepository<AccountingEntity, UUID> {
+
+    /**
+     * Find all items according to email and isActive.
+     *
+     * @param email string that is the email of the accountant
+     * @param isActive boolean type that shows whether the items are active or not.
+     * @return List of AccountingEntity. Please, see the {@link AccountingEntity} for more details.
+     *
+     * @author <a href="https://github.com/evrentan">Evren Tan</a>
+     * @since 1.0.0
+     */
     List<AccountingEntity> findAllByEmailAndActive(String email, Boolean isActive);
 }
